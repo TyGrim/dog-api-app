5827fabaf63d8f4069e485a2046abb529f54ecaf
TyGrim <tysongyorfy@gmail.com>
2022-02-07 10:43:05 -0800

    style .scss button form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <section className="Controls">
+        <h1>Who let the Dogs out?</h1>
+        <form>
+          <button type="button">+1</button>
+          <button type="button">+5</button>
+          <button type="button">-1</button>
+          <button type="button">-5</button>
+        </form>
+      </section>
+      <section className="Gallery">
+        <div className="ImageBox"></div>
+        <div className="ImageBox"></div>
+        <div className="ImageBox"></div>
+        <div className="ImageBox"></div>
+        <div className="ImageBox"></div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
